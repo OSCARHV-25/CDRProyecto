217151822761de3a575dc7a82bab542a93b660c6
adonay <strlemuszu@upana.edu.gt>
2024-09-24 00:48:28 -0600

    Reconstruyendo el Productor y el Buffer Comparitdo

--- src/main/java/cdr_proyecto/proyecto/BufferCompartido.java
+++ src/main/java/cdr_proyecto/proyecto/BufferCompartido.java
@@ -1,23 +1,22 @@
 package cdr_proyecto.proyecto;
-
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class BufferCompartido {
-    private final BlockingQueue<String> queue; // cola que maneja los valores
+    private final BlockingQueue<String> cola; // cola que maneja los valores
 
     public BufferCompartido(int capacidad) {
-        this.queue = new ArrayBlockingQueue<String>(capacidad);
+        this.cola = new ArrayBlockingQueue<String>(capacidad);
     }
 
     public void producir(String item) throws InterruptedException {
-        queue.put(item); // El put para producir un item o un numero
+        cola.put(item); // El put para producir un item en este caso una linea
         System.out.println("Elemento producido: " + item);
     }
 
 
     public String consumir() throws InterruptedException { // el String porque consumir si nos devolvera algo
-        String item = queue.take();//El take es para tomar o consumir un numero de la cola el primero en la cola
+        String item = cola.take();//El take es para tomar o consumir la linea de la cola
         System.out.println("Elemento consumido: " + item);
         return item; // tiene que retornar el valor que esta retornando de la cola
 
